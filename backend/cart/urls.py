9fbcaf80ece75f46848b1ac685bdc14f4e745bfe
BonaneN <rbonane336@gmail.com>
2026-04-02 12:53:08 +0200

    chore: remove entire backend directory and all associated source files

--- backend/cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CartViewSet
-
-urlpatterns = [
-    path('view-my-cart/', CartViewSet.as_view({'get': 'my_cart'}), name='cart-view'),
-    path('add-item-to-cart/', CartViewSet.as_view({'post': 'add_item'}), name='cart-add'),
-    path('remove-item-from-cart/', CartViewSet.as_view({'delete': 'remove_item'}), name='cart-remove'),
-    path('update-item-quantity/', CartViewSet.as_view({'patch': 'update_quantity'}), name='cart-update'),
-]
